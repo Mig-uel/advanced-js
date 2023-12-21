4131de33708d94d2dc29c5ca80e7035f4b886093
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-21 15:40:07 -0500

    Refactor Cat class and add factory method for registering strays

--- OOP/classes/Cat.js
+++ OOP/classes/Cat.js
@@ -6,7 +6,25 @@ export class Cat {
 
   static genusSpecies = 'felis catus'
 
-  static meow() {
-    return 'MEOW MEOW MEOW'
+  // factory function/methods
+  static registerStray() {
+    const names = ['Muffin', 'Biscuit', 'Sleepy', 'Dodo', 'Princess Butterface']
+
+    const name = choice(names)
+
+    return new Cat(name, 'unknown')
   }
+
+  // static meow() {
+  //   return 'MEOW MEOW MEOW'
+  // }
+
+  meow() {
+    return `${this.name} says meow`
+  }
+}
+
+function choice(arr) {
+  const randIndex = Math.floor(Math.random() * arr.length)
+  return arr[randIndex]
 }
