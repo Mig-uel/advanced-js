8d6f40c934d7b66c782918b7f4119522779d62e5
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-19 19:44:32 -0400

    feat: Add Cat class with basic properties and methods

---
+++ public-fields.js
@@ -0,0 +1,19 @@
+class Cat {
+  static numOfCats = 0
+
+  // class fields
+  name
+  breed
+  numLegs = 4
+  hasTail = true
+
+  constructor(name, breed) {
+    this.name = name
+    this.breed = breed
+    Cat.numOfCats += 1
+  }
+
+  amputate() {
+    this.numLegs -= 1
+  }
+}
