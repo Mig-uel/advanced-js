64bb88c45afa1ff25922203bbbedcbf957f0f541
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-11 20:21:33 -0500

    feat: Implement initial infinite scrolling logic
    - Adds a function to load more items when scrolling near the bottom
    - Utilizes random colors for newly created items

---
+++ timers/throttling/throttling.js
@@ -0,0 +1,44 @@
+function getRandomColor() {
+  const palette = [
+    '#FFADAD',
+    '#FFD6A5',
+    '#FDFFB6',
+    '#CAFFBF',
+    '#9BF6FF',
+    '#A0C4FF',
+    '#BDB2FF',
+    '#FFC6FF',
+  ]
+
+  const randomIndex = Math.floor(Math.random() * palette.length)
+
+  return palette[randomIndex]
+}
+
+const content = document.getElementById('content')
+
+function loadMoreItems() {
+  const scrollDistanceToBottom =
+    document.documentElement.scrollHeight - window.scrollY - window.innerHeight
+
+  if (scrollDistanceToBottom < 200) {
+    console.log('LOADING DATA FROM API')
+
+    for (let i = 0; i < 10; i++) {
+      const item = document.createElement('div')
+
+      item.classList.add('item')
+      item.textContent = 'Item ' + (content.children.length + 1)
+      item.style.background = getRandomColor()
+
+      content.appendChild(item)
+    }
+  }
+}
+
+window.addEventListener('scroll', () => {
+  loadMoreItems()
+})
+
+// initial load
+loadMoreItems()
