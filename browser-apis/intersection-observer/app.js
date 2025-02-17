9339369b4cfd232a4f584b44427b7acf2414cc3b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-16 19:22:03 -0500

    feat: Enhance ad visibility tracking with duration logging
    - Implemented tracking of ad view time using Intersection Observer
    - Added logic to calculate and log the duration an ad is visible

--- browser-apis/intersection-observer/app.js
+++ browser-apis/intersection-observer/app.js
@@ -1,22 +1,53 @@
-const options = {
-  threshold: [0, 0.25, 0.5, 0.75, 1],
-}
+// const options = {
+//   threshold: [0, 0.25, 0.5, 0.75, 1],
+// }
 
-function observerCB(entries) {
-  entries.forEach((entry) => {
-    // console.log('INTERSECTION OBSERVED')
-    // console.log(entry)
+// function observerCB(entries) {
+//   entries.forEach((entry) => {
+//     // console.log('INTERSECTION OBSERVED')
+//     // console.log(entry)
 
-    if (entry.isIntersecting) {
-      const percentage = Math.round(entry.intersectionRatio * 100, 2)
-      console.log(`${percentage}% of the ad is visible`)
-    } else {
-      console.log('AD IS NOT VISIBLE')
-    }
-  })
-}
+//     if (entry.isIntersecting) {
+//       const percentage = Math.round(entry.intersectionRatio * 100, 2)
+//       console.log(`${percentage}% of the ad is visible`)
+//     } else {
+//       console.log('AD IS NOT VISIBLE')
+//     }
+//   })
+// }
 
-const observer = new IntersectionObserver(observerCB, options)
+// const observer = new IntersectionObserver(observerCB, options)
+
+// const ad = document.getElementById('ad')
+// observer.observe(ad)
+
+/** Tracking Ad View Time */
+let adViewTimes = []
+let adVisibleStartTime
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const { isIntersecting } = entry
+
+      if (isIntersecting) {
+        // ad is visible
+        adVisibleStartTime = Date.now()
+      } else if (adVisibleStartTime) {
+        // ad was visible, no longer is visible
+        let adViewDuration = Date.now() - adVisibleStartTime
+        adViewTimes.push(adViewDuration)
+
+        console.log(`Ad was viewed for ${adViewDuration}ms`)
+
+        adViewDuration = undefined
+      }
+    })
+  },
+  {
+    threshold: 0.5,
+  }
+)
 
 const ad = document.getElementById('ad')
 observer.observe(ad)
