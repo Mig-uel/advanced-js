1a4b60173b028a0ead0d10366bf41a23f3dfc35d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-17 21:49:37 -0500

    feat: Add ball movement and animation to canvas app
    - Implement ball class with velocity for movement
    - Add update method to handle ball position changes
    - Introduce animation loop for continuous rendering

--- browser-apis/bouncing-balls/app.js
+++ browser-apis/bouncing-balls/app.js
@@ -4,12 +4,15 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 const ctx = canvas.getContext('2d')
+const balls = []
 
 /** Ball Class */
 class Ball {
   constructor(x, y) {
     this.x = x
     this.y = y
+    this.x_velocity = (Math.random() - 0.5) * 10
+    this.y_velocity = (Math.random() - 0.5) * 10
     this.endAngle = Math.PI * 2
     this.size = Math.random() * 30 + 10
     this.color = Ball.getRandomColor()
@@ -29,6 +32,23 @@ class Ball {
     ctx.arc(this.x, this.y, this.size, 0, this.endAngle)
     ctx.fill()
   }
+
+  /** Update ball coords */
+  update() {
+    this.x += this.x_velocity
+    this.y += this.y_velocity
+  }
+}
+
+function loop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  for (let ball of balls) {
+    ball.update()
+    ball.draw()
+  }
+
+  requestAnimationFrame(loop)
 }
 
 canvas.addEventListener('click', (e) => {
@@ -37,5 +57,7 @@ canvas.addEventListener('click', (e) => {
     y: e.clientY,
   }
 
-  new Ball(cords.x, cords.y).draw()
+  const ball = new Ball(cords.x, cords.y)
+  balls.push(ball)
+  loop()
 })
